7eaeba0f1468527493a2be74c26613ded6f6183f
goosedum <154931486+goosedum@users.noreply.github.com>
2024-09-19 18:26:48 +0300

    m

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,22 +4,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
 
-// const INITIAL_STATE = {
-//     items: [
-//         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-// };
+
 
 const getInitialContacts = () => {
   const savedContacts = localStorage.getItem('contacts');
   return savedContacts ? JSON.parse(savedContacts) : [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    { id: 'id-1', name: 'Gososedum', number: '909090' },
   ];
 };
 
